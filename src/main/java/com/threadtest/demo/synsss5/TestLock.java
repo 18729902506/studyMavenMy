6db85cd703f0fc7cb214b82d85ad6b3bf6b0cfcf
ssss <shang_rongrong@icekredit.com>
2020-11-02 00:20:44 +0800

    feat：Lock锁

---
+++ src/main/java/com/threadtest/demo/synsss5/TestLock.java
@@ -0,0 +1,45 @@
+package com.threadtest.demo.synsss5;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TestLock {
+    public static void main(String[] args) {
+        TestLock2 testLock2 = new TestLock2();
+        new Thread(testLock2).start();
+        new Thread(testLock2).start();
+        new Thread(testLock2).start();
+    }
+}
+
+class TestLock2 implements Runnable {
+
+    int ticketNums = 10;
+    /**
+     * 定义lock锁
+     * 一个一个出来，相当于排队的概念
+     */
+    private final ReentrantLock lock = new ReentrantLock();
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                //加锁    显示的定义锁，随便枷锁它的区域    与synchronized（隐式）的区别
+                lock.lock();
+                if (ticketNums > 0) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(ticketNums--);
+                } else {
+                    break;
+                }
+            } finally {
+                //解锁
+                lock.unlock();
+            }
+        }
+    }
+}
