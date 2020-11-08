f14dc4829c9ec98aa15f7a5dee79d5fb42dd6a1a
ssss <shang_rongrong@icekredit.com>
2020-11-08 22:42:28 +0800

    feat：初始JUC

---
+++ src/main/java/com/juctest/demo/Test1.java
@@ -0,0 +1,12 @@
+package com.juctest.demo;
+
+/***
+ * 查看CPU的核数
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        //8  获取CPU的核数
+        //CPU密集型  IO密集型
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
